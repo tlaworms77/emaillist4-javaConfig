d63c52e2ffb66012dfe1ea68d6215c52d7e5b038
tlawo <tlawo@LAPTOP-ICSS4D7B>
2019-03-20 15:58:49 +0900

    EmailList 4 <Java Config 처리>

---
+++ src/main/java/com/douzone/emaillist/config/RootConfig.java
@@ -0,0 +1,10 @@
+package com.douzone.emaillist.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.douzone.emaillist.dao")
+public class RootConfig {
+
+}
